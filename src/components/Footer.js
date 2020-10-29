66308930c0900eef41a171d240269005dc5dc510
Zeyesa Hinji <42301959+hinjizeyesa@users.noreply.github.com>
2020-10-29 13:45:32 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = (props) => (
+    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+        
+        <p className="copyright">
+        Zeyesa Hinji
+        <br/>
+        </p>
+    </footer>
+)
+
+Footer.propTypes = {
+    timeout: PropTypes.bool
+}
+
+export default Footer
